dcbdbb2113ed1a00b1df9ffcd2b2bf0b5c3ef62e
koshikawa <koshikawa@ppworks.jp>
2014-07-09 17:27:05 +0900

    Separate rails_helper from spec_helper

--- spec/features/my/account_byebyes_spec.rb
+++ spec/features/my/account_byebyes_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require 'rails_helper'
 
 describe 'My/Byebyes' do
   let(:user) { create(:user) }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,101 +1,58 @@
-require 'rubygems'
-
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'json_expressions/rspec'
-
-# Load all railties files
-Rails.application.railties.to_a { |r| r.eager_load! }
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = :random
 
-  # Use file location for spec type
-  config.infer_spec_type_from_file_location!
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
 
-  # For capybara
-  require 'capybara/rspec'
-  require 'capybara/poltergeist'
-  Capybara.register_driver :rack_test do |app|
-      Capybara::RackTest::Driver.new(app, headers: {'HTTP_ACCEPT_LANGUAGE' => 'ja-JP'})
-  end
-  Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, timeout: 360)
-  end
-  Capybara.javascript_driver = :poltergeist
-
-  # For database cleaner
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+    expectations.syntax = :expect
   end
 
-  config.before(:each) do |example|
-    if example.metadata[:js]
-      page.driver.resize(1024, 2048)
-    end
-    I18n.locale = :en
-    if Capybara.current_driver == :poltergeist
-      # poltergeistでは
-      # Capybara::Poltergeist::Driver.new で指定できないので
-      Capybara.current_session.driver.headers = { 'Accept-Language' => I18n.locale.to_s }
-    end
-    DatabaseCleaner.strategy = :truncation
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
   end
-
-  config.after(:each) do |example|
-    DatabaseCleaner.clean
-    if example.metadata[:js]
-      load "#{Rails.root}/db/seeds.rb"
-    end
-  end
-
-  # macro
-  config.include RoutingResourcesMacros, type: :routing
-  config.include FeatureMacros, type: :feature
-  config.include FactoryGirl::Syntax::Methods
-  config.include Delorean
-
-  config.before(:all) do
-    FactoryGirl.reload
-  end
-
-  # faker
-  Faker::Config.locale = :en
-
-  # master data
-  load "#{Rails.root}/db/seeds.rb"
 end
